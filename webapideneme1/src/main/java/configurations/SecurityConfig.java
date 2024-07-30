8edfd16631e90c92a820b1b15ef97f982acac064
baylinux01 <baylinux01@gmail.com>
2024-07-30 16:14:55 +0300

    security için bazı temel configürasyonlar eklendi

---
+++ webapideneme1/src/main/java/configurations/SecurityConfig.java
@@ -0,0 +1,29 @@
+package configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		return http
+				.csrf(customizer->customizer.disable())
+				.authorizeHttpRequests(req->req.anyRequest().authenticated())
+			  //.formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
+				
+	}
+	
+
+}
