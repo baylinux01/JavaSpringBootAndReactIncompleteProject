28c806a0f04fe751282894b53114800783263f55
baylinux01 <baylinux01@gmail.com>
2024-07-31 15:01:19 +0300

    @EnableTransactionManagement annotation added

--- webapideneme1/src/main/java/com/demo/webapideneme1/Webapideneme1Application.java
+++ webapideneme1/src/main/java/com/demo/webapideneme1/Webapideneme1Application.java
@@ -2,8 +2,10 @@ package com.demo.webapideneme1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Webapideneme1Application {
 
 	public static void main(String[] args) {
